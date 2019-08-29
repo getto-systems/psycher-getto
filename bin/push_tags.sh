5f5020c80afd9a34a818fdd23c7d8b7a488d66a8
shun <shun.fix9@gmail.com>
2019-08-29 19:57:19 +0000

    fix: push_tags

--- bin/push_tags.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git remote add super https://getto-systems:$GITLAB_ACCESS_TOKEN@gitlab.com/getto-systems-base/psycher/getto.git
-git remote add maint https://getto-systems:$GITHUB_ACCESS_TOKEN@github.com/getto-systems/psycher-getto.git
-git tag $(cat .release-version)
-git push super HEAD:master --tags
-git push maint HEAD:master --tags
-git push maint HEAD:maintenance -f
